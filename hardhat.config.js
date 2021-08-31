b097a19f6545988a4a9bc67ded31682fded03a3f
Satyam Bansal <satyam.b@turing.com>
2021-08-31 10:05:12 +0530

    Remove tsconfig file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,34 +1,35 @@
-require('@nomiclabs/hardhat-waffle');
-require('dotenv/config');
+require("@nomiclabs/hardhat-waffle")
+require("dotenv/config")
 
-const fs = require('fs');
-// const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789";
+const fs = require("fs")
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
 // const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
 
-console.log('env:: ', process.env.FORKING);
-console.log('env:: ', process.env.ALCHEMY_API_KEY);
+console.log("env:: ", process.env.FORKING)
+console.log("env:: ", process.env.ALCHEMY_API_KEY)
 
 module.exports = {
-  defaultNetwork: 'hardhat',
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337,
       forking: {
-        enabled: process.env.FORKING === 'true',
+        enabled: process.env.FORKING === "true",
         url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
       },
       live: false,
       saveDeployments: true,
-      tags: ['test', 'local'],
+      tags: ["test", "local"],
       blockNumber: 11095000,
     },
-    /*
+
     mumbai: {
       // Infura
       // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
-      url: "https://rpc-mumbai.matic.today",
-      accounts: [privateKey]
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [privateKey],
     },
+    /*
     matic: {
       // Infura
       // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
@@ -38,7 +39,7 @@ module.exports = {
     */
   },
   solidity: {
-    version: '0.8.4',
+    version: "0.8.4",
     settings: {
       optimizer: {
         enabled: true,
@@ -47,12 +48,12 @@ module.exports = {
     },
   },
   paths: {
-    sources: './contracts',
-    tests: './test',
-    cache: './cache',
-    artifacts: './artifacts',
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
   },
-};
+}
 
 /*
 JSON-RPC based network configuration
